46b49b5517162d243d8c4c1ebb4813ecb9b46825
Daniil <94884910+Filienko@users.noreply.github.com>
2023-02-24 21:08:51 -0800

    Added done changes

--- prodcons_test.c
+++ prodcons_test.c
@@ -37,29 +37,44 @@ Matrix* get()
 // Matrix PRODUCER worker thread
 void *prod_worker(void *matrix_prod)
 {  
-
+  //referencing passed global counter of produced matrix 
   counter_t* matrix_produced = matrix_prod;
+
+  //Initialize necessary minimum values for the statistics of that threat, located on the heap
   ProdConsStats* produced_stats = malloc(sizeof(ProdConsStats));
   produced_stats->sumtotal = 0;
   produced_stats->matrixtotal = 0;
   produced_stats->multtotal = 0;
   int done = 0;
 
+  //If sufficient amount of matrices was produced, do not put additional matrix or acquire the lock, terminate
+  if(get_cnt(matrix_produced) >= NUMBER_OF_MATRICES)
+  {
+    done = 1; 
+  }
+
   while(!done)
   {
     Matrix* matrix = GenMatrixRandom();
+    //Critical section
     pthread_mutex_lock(&mutex);
 
+    //If sufficient amount of matrices was produced, do not put additional matrix
     if(get_cnt(matrix_produced) >= NUMBER_OF_MATRICES)
     {
       done = 1;
     }
 
     if (!done) {
+      //If sufficient amount of matrices was not produced, and the buffer is full,
+      //wait for a signal
       while (count == BOUNDED_BUFFER_SIZE && get_cnt(matrix_produced) < NUMBER_OF_MATRICES) {
         pthread_cond_wait(&empty, &mutex);
       }
-      if(get_cnt(matrix_produced) < NUMBER_OF_MATRICES)
+      
+      //If sufficient amount of matrices was not produced, and the buffer is not full
+      // allows to put 1 matrix and update the counters and statistics
+      if(count < BOUNDED_BUFFER_SIZE && get_cnt(matrix_produced) < NUMBER_OF_MATRICES)
       {
         put(matrix);
         produced_stats->sumtotal += SumMatrix(matrix);
@@ -69,11 +84,14 @@ void *prod_worker(void *matrix_prod)
       }
     }
 
+    //If no more matrices id needed, send a signal and release the lock.
     if (done) {
       pthread_cond_signal(&empty);
       printf("\nINNER Producer, %i\n",produced_stats->matrixtotal);
       printf("\nINNER Producer, %i\n",produced_stats->sumtotal);
     }
+
+    //Release the lock before returning a thread or acquiring the lock again before putting another matrix
     pthread_mutex_unlock(&mutex);
   }
 
@@ -84,45 +102,66 @@ void *prod_worker(void *matrix_prod)
 // Matrix CONSUMER worker thread
 void *cons_worker(void *matrix_cons)
 {
+  //referencing passed global counter of produced matrix 
   counter_t* matrix_consumed = matrix_cons;
+
+  //Initialize necessary minimum values for the statistics of that threat, located on the heap
   ProdConsStats* consumed_stats = malloc(sizeof(ProdConsStats));
   consumed_stats->sumtotal = 0;
   consumed_stats->matrixtotal = 0;
   consumed_stats->multtotal = 0;
   int done = 0;
 
+  //State the pointer for Matrices
   Matrix* M1 = NULL;
   Matrix* M2 = NULL;
   Matrix* M3 = NULL;
   
+  //If sufficient amount of matrices was consumed, do not acquire additional matrix
+  if(get_cnt(matrix_consumed) >= NUMBER_OF_MATRICES)
+  {
+    done = 1;
+  }
+
   while(!done)
   {
+    //Acquire the lock for the first matrix
     pthread_mutex_lock(&mutex);
 
-  
+    //If sufficient amount of matrices was not consumed, and the buffer is empty,
+    //wait for a signal
     while (count == 0 && get_cnt(matrix_consumed) < NUMBER_OF_MATRICES) {
       pthread_cond_wait(&fill, &mutex);
     }
 
-    if(count != 0 & get_cnt(matrix_consumed) < NUMBER_OF_MATRICES)
+    //If sufficient amount of matrices was not consumed, and the buffer is not empty
+    // allows to get matrix and update the counters and statistics
+    if(count > 0 && (get_cnt(matrix_consumed) < NUMBER_OF_MATRICES))
     {
       M1 = get();
-      consumed_stats->sumtotal += SumMatrix(M1);
       increment_cnt(matrix_consumed);
-      pthread_cond_signal(&empty);
+      consumed_stats->sumtotal += SumMatrix(M1);
       consumed_stats->matrixtotal++;
+      pthread_cond_signal(&empty);
     }
-
+    //Release the lock before attemting to acquire second matrix
     pthread_mutex_unlock(&mutex);
 
     if (M1) {
+      //If M1 is available, repeat until either no more matrices are to be consumed or M3 is acquried
       while (!(done || M3)) {
+        //Acquire the lock for the second matrix attempt
         pthread_mutex_lock(&mutex);      
         
-        while (count == 0 && get_cnt(matrix_consumed) < NUMBER_OF_MATRICES) {
+        //If sufficient amount of matrices was not consumed, and the buffer is empty,
+        //wait for a signal
+        while (count == 0 && get_cnt(matrix_consumed) < NUMBER_OF_MATRICES) 
+        {
           pthread_cond_wait(&fill, &mutex);
         }
-
+        
+        //If sufficient amount of matrices was not consumed, and the buffer is not empty
+        // allows to get second matrix for multiplication, then update the counters and statistics
         if(count > 0 && get_cnt(matrix_consumed) < NUMBER_OF_MATRICES)
         {
           M2 = get();
@@ -138,43 +177,46 @@ void *cons_worker(void *matrix_cons)
             M2 = NULL;
           }
         }
-
+        //If sufficient amount of matrices was consumed, do not get additional matrix
         if(get_cnt(matrix_consumed) >= NUMBER_OF_MATRICES)
         {
           done = 1;
         }
-
-        pthread_mutex_unlock(&mutex);
       } 
     }
     
+    //Free matrices that were acquired
     if (M1) {
       FreeMatrix(M1);
       M1 = NULL;
     }
-    if (M2) {
+    if (M2) 
+    {
       FreeMatrix(M2);
       M2 = NULL;
     }
     if (M3) {
       DisplayMatrix(M3,stdout);
-
+      consumed_stats->multtotal++;
       FreeMatrix(M3);
       M3 = NULL;
     }
 
-
+    //If no more matrices are to be produced, done, release the lock later.
     if(get_cnt(matrix_consumed) >= NUMBER_OF_MATRICES)
     {
-      done = 1;
+      done = 1;  
+    }
+    else
+    {
+      //Unlock the mutex to acuqire it at the start of the while body
+      pthread_mutex_unlock(&mutex);  
     }
 
-    consumed_stats->multtotal++;
   }
-
-  
   pthread_cond_signal(&fill);
   pthread_mutex_unlock(&mutex);
+
   printf("\nOUTER Consumer, %i\n",consumed_stats->matrixtotal);
   printf("\nOUTER Consumer, %i\n",consumed_stats->sumtotal);
   return consumed_stats;

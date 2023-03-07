587d8b80ca9d6272bf08f89c1deb7421190e2beb
Daniil <94884910+Filienko@users.noreply.github.com>
2023-03-07 12:16:52 -0800

    Update helloModule.c

--- helloModule.c
+++ helloModule.c
@@ -4,29 +4,38 @@
 #include<asm/io.h>
 
 // TOTALS structures
-typedef struct counter {
-  int total_con;
-  int total_noncon;
-  int total_pages;
-} Stats;
+struct Stats {
+  unsigned long total_con;
+  unsigned long total_noncon;
+  unsigned long total_pages;
+};
 
+struct Stats proc_count(void);
 
-int proc_init (void) {
+int proc_init(void) {
   struct Stats counter_totals;
+  counter_totals.total_con = 0;
+  counter_totals.total_noncon = 0;
+  counter_totals.total_pages = 0;
+
   printk(KERN_INFO "PROCESS REPORT:\n");
   printk(KERN_INFO "proc_id,proc_name,contig_pages,noncontig_pages,total_pages\n");
   counter_totals = proc_count();
-  printk(KERN_INFO "TOTALS,, %d,%d,%d\n",
-    counter_totals->total_con,counter_totals->total_noncon,counter_totals->total_pages);
+  printk(KERN_INFO "TOTALS,, %lu,%lu,%lu\n",
+    counter_totals.total_con,counter_totals.total_noncon,counter_totals.total_pages);
   return 0;
 }
 
-struct Stats pages_count(struct task_struct task)
+MODULE_LICENSE("GPL");
+module_init(proc_init);
+module_exit(proc_cleanup);
+
+static struct Stats pages_count(struct task_struct* task)
 {
   struct Stats pages_totals;
-  pages_totals->total_con = 0;
-  pages_totals->total_noncon = 0;
-  pages_totals->total_pages = 0;
+  pages_totals.total_con = 0;
+  pages_totals.total_noncon = 0;
+  pages_totals.total_pages = 0;
   int temp_con = 0;
   int temp_noncon = 0;
   struct vm_area_struct *vma = 0;
@@ -39,14 +48,14 @@ struct Stats pages_count(struct task_struct task)
         {
           //HIGH LEVEL LOGIC
           temp_con += 1;
-          pages_totals->total_noncon = (((pages_totals->total_noncon) >= (temp_noncon)) ? (pages_totals->total_noncon) : (temp_noncon));
+          pages_totals.total_noncon = (((pages_totals.total_noncon) >= (temp_noncon)) ? (pages_totals.total_noncon) : (temp_noncon));
           temp_noncon = 0;
         }
         else
         {
           //HIGH LEVEL LOGIC
           temp_noncon += 1;
-          pages_totals->total_con = (((pages_totals->total_con) >= (temp_con)) ? (pages_totals->total_con) : (temp_con));
+          pages_totals.total_con = (((pages_totals.total_con) >= (temp_con)) ? (pages_totals.total_con) : (temp_con));
           temp_con = 0;
         }
       }
@@ -58,44 +67,40 @@ void proc_cleanup(void) {
   printk(KERN_INFO "helloModule: performing cleanup of module\n");
 }
 
-MODULE_LICENSE("GPL");
-module_init(proc_init);
-module_exit(proc_cleanup);
-
-struct Stats proc_count(void)
+struct Stats proc_count()
 {
   struct Stats counter_totals;
-  counter_totals->total_con = 0;
-  counter_totals->total_noncon = 0;
-  counter_totals->total_pages = 0;
+  counter_totals.total_con = 0;
+  counter_totals.total_noncon = 0;
+  counter_totals.total_pages = 0;
 
   struct task_struct *thechild;
   for_each_process(thechild)
   {
-    if(task_struct.pid > 650)
+    if(thechild->pid > 650)
     {
-      pages_count(task_struct);
+      pages_count(thechild);
       //Process ID
-      proc_id = thechild.pid;
+      pid_t proc_id = thechild->pid;
       //Process name
-      proc_name = thechild.comm;
-      //Total number of contiguously memory pages allocated for the process
-      total_con_pages = thechild.;
-      //Total number of non-contiguously memory pages allocated for the process
-      total_noncon_pages = thechild.;
+      char *proc_name = thechild->comm;
       //Total number of memory pages allocated for the process
-      total_pages = thechild.;
+      unsigned long total_pages = thechild->mm->total_vm;
+      //Total number of contiguously memory pages allocated for the process
+      unsigned long total_con_pages = total_pages - thechild->mm->nr_ptes;
+      //Total number of non-contiguously memory pages allocated for the process
+      unsigned longt total_noncon_pages = thechild->mm->nr_ptes;
 
-      printk(KERN_INFO "%d, %c, %d, %d, %d\n", proc_id, proc_name, total_con_pages,total_noncon_pages,total_pages);
-      counter_totals->total_con += total_con_pages;
-      counter_totals->total_noncon += total_noncon_pages;
-      counter_totals->total_pages += total_pages;
+      printk(KERN_INFO "%lu, %s, %lu, %lu, %lu\n", proc_id, proc_name, total_con_pages,total_noncon_pages,total_pages);
+      counter_totals.total_con += total_con_pages;
+      counter_totals.total_noncon += total_noncon_pages;
+      counter_totals.total_pages += total_pages;
     }
   }
  return counter_totals;
 }
 
-int virt2phys(struct task_struct task, unsigned long virt)
+int virt2phys(task_struct* task, unsigned long virt)
 {
   pgd_t *p4d;
   pgd_t *pud;
